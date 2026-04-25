acc3dfe7d75fb44346a72124ac795914b3bc3865
Sahil Chouhan <chouhangolu604@gmail.com>
2026-04-25 07:52:37 +0530

    update

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -3,8 +3,13 @@ import { useState } from "react"
 
 const AddTodo = () => {
     const [todo, setTodo] = useState("")
+    const handleFormSubmit = (e:React.SubmitEvent<HTMLFormElement>)=>{
+      e.preventDefault()
+      handleAddToDo(todo)
+      setTodo("")
+    }
   return (
-    <form >
+    <form onSubmit={}>
 <input type="text" name="" value={todo} onChange={(e)=>setTodo(e.target.value)} />
 <button type="submit">Add</button>
     </form>
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -3,3 +3,8 @@ import { createContext } from "react";
 
 export const todosContext = createContext(null)
 
+export const TodosProvider = ({ children }) => {
+    return <todosContext.Provider>
+        {children}
+    </todosContext.Provider>
+}
